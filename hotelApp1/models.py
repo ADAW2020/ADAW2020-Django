e4306423ce245421e7f26c87c67058ee4c470d7c
Yamil <ymuza993@alumnos.iua.edu.ar>
2020-10-29 10:51:01 -0300

    db fix

--- hotelApp1/models.py
+++ hotelApp1/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
-  
-class Habitacion(models.Model):
-    precio = models.IntegerField(default=100)    
-    numero = models.IntegerField()
-    tipo = models.CharField(max_length=250)
 
 class tipoHabitacion(models.Model):
     descripcion = models.CharField(max_length=40)
+  
+class Habitacion(models.Model):
+    precio = models.IntegerField()    
+    numero = models.IntegerField()
+    tipo = models.ForeignKey(tipoHabitacion, on_delete=models.CASCADE)
+    estado = models.BooleanField()
+
 
 
 class Reservas(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha_desde = models.CharField(max_length=250)
     fecha_hasta = models.CharField(max_length=250)
-    codigo_reserva = models.AutoField(primary_key=True) # 1 => codigo reserva por default
-    estado = models.CharField(max_length=250, default='activa')
+    codigo_reserva = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) #  codigo reserva por default autogenerado
+    estado = models.BooleanField()
     cant_huespedes = models.IntegerField()
-    cant_habitaciones = models.IntegerField(default=1)
+    cant_habitaciones = models.IntegerField()
     precio_total = models.FloatField()
+    Habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE)
     
